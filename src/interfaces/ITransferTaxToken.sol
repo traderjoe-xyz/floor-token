542312a064d1cda8b94c24e04cb575ddeb93a43b
0x0Louis <louis@joepegs.com>
2023-05-11 17:46:55 +0200

    create basic transfer tax token

---
+++ src/interfaces/ITransferTaxToken.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {IERC20} from "openzeppelin-contracts/token/ERC20/IERC20.sol";
+
+interface ITransferTaxToken {
+    event TaxRecipientSet(address indexed taxRecipient);
+
+    event TaxRateSet(uint256 taxRate);
+
+    event ExcludedFromTaxSet(address indexed account, bool excluded);
+
+    function taxRecipient() external view returns (address);
+
+    function taxRate() external view returns (uint256);
+
+    function excludedFromTax(address account) external view returns (bool);
+
+    function mint(address to, uint256 amount) external;
+
+    function setTaxRate(uint256 taxRate) external;
+
+    function setTaxRecipient(address taxRecipient) external;
+
+    function setExcludedFromTax(address account, bool excluded) external;
+}
