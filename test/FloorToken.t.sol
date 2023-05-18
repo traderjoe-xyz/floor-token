1d83d4f3bb9ffbe8491a17c94b5bd197cf77f891
Louis <107303182+0x0Louis@users.noreply.github.com>
2023-05-18 14:15:32 +0200

    Floor token (#14)
    * add safe rebalance
    
    * avoid underflow error
    
    * typos
    
    * Safe rebalance (#13)
    
    * use constants
    
    * typos
    
    * safer raise roof
    
    * test that rebalance and raiseRoof doesn't steal tokens
    
    ---------
    
    Co-authored-by: robotmurloc <81496776+0xmurloc@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: robotmurloc <81496776+0xmurloc@users.noreply.github.com>

--- test/FloorToken.t.sol
+++ test/FloorToken.t.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.0;
 import "forge-std/Test.sol";
 import "openzeppelin-contracts/utils/math/Math.sol";
 import {ERC20} from "openzeppelin-contracts/token/ERC20/ERC20.sol";
+import {ILBRouter} from "joe-v2/interfaces/ILBRouter.sol";
 
 import {FloorToken, IWNATIVE, ILBFactory, ILBPair, IERC20} from "src/FloorToken.sol";
 
@@ -12,6 +13,7 @@ contract TransferTaxFloorTokenTest is Test {
 
     IWNATIVE public constant wNative = IWNATIVE(0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7);
     ILBFactory public constant lbFactory = ILBFactory(0x8e42f2F4101563bF679975178e880FD87d3eFd4e);
+    ILBRouter public constant lbRouter = ILBRouter(0xb4315e873dBcf96Ffd0acd8EA43f689D8c20fB30);
     uint256 constant tokenPerBin = 100e18;
     uint24 constant initId = 1 << 23;
     uint16 constant binStep = 25;
@@ -106,11 +108,73 @@ contract TransferTaxFloorTokenTest is Test {
             assertEq(share.mulDiv(binReserveY, totalShares), 0, "test_RaiseRoof::6");
         }
 
-        vm.expectRevert("FloorToken: invalid nbBins");
+        vm.expectRevert("FloorToken: zero bins");
         token.raiseRoof(0);
 
-        vm.expectRevert("FloorToken: invalid nbBins");
+        vm.expectRevert("FloorToken: new roof too high");
         token.raiseRoof(type(uint24).max - roofId);
+
+        vm.expectRevert("FloorToken: new roof too high");
+        token.raiseRoof(100 - (roofId - floorId) + 1);
+
+        token.raiseRoof(100 - (roofId - floorId));
+    }
+
+    function test_RaiseRoofWithTokenInThePair() public {
+        ILBPair pair = ILBPair(token.pair());
+
+        (uint256 tokenReserves, uint256 wNativeReserves) = pair.getReserves();
+        (uint256 tokenProtocolFees, uint256 wNativeProtocolFees) = pair.getProtocolFees();
+
+        uint256 amount = 1;
+
+        deal(address(token), address(this), amount);
+        deal(address(wNative), address(this), amount);
+
+        token.transfer(address(pair), amount);
+        wNative.transfer(address(pair), amount);
+
+        token.raiseRoof(10);
+
+        uint256 tokenBalance = token.balanceOf(address(pair));
+        uint256 wNativeBalance = wNative.balanceOf(address(pair));
+
+        assertEq(
+            tokenBalance - (tokenReserves + tokenProtocolFees),
+            10 * tokenPerBin + amount,
+            "test_RaiseRoofWithTokenInThePair::1"
+        );
+        assertEq(
+            wNativeBalance - (wNativeReserves + wNativeProtocolFees), amount, "test_RaiseRoofWithTokenInThePair::2"
+        );
+
+        (tokenReserves, wNativeReserves) = pair.getReserves();
+        (tokenProtocolFees, wNativeProtocolFees) = pair.getProtocolFees();
+
+        amount = 10 * tokenPerBin + 1;
+
+        deal(address(token), address(this), amount);
+        deal(address(wNative), address(this), amount);
+
+        token.transfer(address(pair), amount);
+        wNative.transfer(address(pair), amount);
+
+        token.raiseRoof(10);
+
+        tokenBalance = token.balanceOf(address(pair));
+        wNativeBalance = wNative.balanceOf(address(pair));
+
+        // Increase amount by 1, because we already have 1 token in the pair from the previous transfer
+        amount += 1;
+
+        assertEq(
+            tokenBalance - (tokenReserves + tokenProtocolFees),
+            10 * tokenPerBin + amount,
+            "test_RaiseRoofWithTokenInThePair::3"
+        );
+        assertEq(
+            wNativeBalance - (wNativeReserves + wNativeProtocolFees), amount, "test_RaiseRoofWithTokenInThePair::4"
+        );
     }
 
     function test_RebalanceWithHighFees() public {
@@ -205,6 +269,73 @@ contract TransferTaxFloorTokenTest is Test {
         assertEq(newFloorId3, newFloorId2, "test_Rebalance::9");
     }
 
+    function test_RebalanceWhileAddingLiquidity() public {
+        address alice = address(1);
+        ILBPair lbPair = ILBPair(token.pair());
+
+        _swapNbBins(lbPair, false, 10);
+
+        deal(address(wNative), alice, 100_000e18);
+
+        vm.startPrank(alice);
+        wNative.transfer(address(lbPair), 100_000e18);
+
+        // Deal the token directly to the pair to make sure it doesn't trigger any callback
+        deal(address(token), address(lbPair), token.balanceOf(address(lbPair)) + 100_000e18);
+
+        int256[] memory deltaIds = new int256[](3);
+        deltaIds[0] = -1;
+        deltaIds[1] = 0;
+        deltaIds[2] = 1;
+
+        uint256[] memory distributionX = new uint256[](3);
+        distributionX[0] = 0;
+        distributionX[1] = 0.5e18;
+        distributionX[2] = 0.5e18;
+
+        uint256[] memory distributionY = new uint256[](3);
+        distributionY[0] = 0.5e18;
+        distributionY[1] = 0.5e18;
+        distributionY[2] = 0;
+
+        ILBRouter.LiquidityParameters memory params = ILBRouter.LiquidityParameters({
+            tokenX: IERC20(address(token)),
+            tokenY: IERC20(address(wNative)),
+            binStep: binStep,
+            amountX: 0,
+            amountY: 0,
+            amountXMin: 100_000e18,
+            amountYMin: 100_000e18,
+            activeIdDesired: lbPair.getActiveId(),
+            idSlippage: 0,
+            deltaIds: deltaIds,
+            distributionX: distributionX,
+            distributionY: distributionY,
+            to: alice,
+            refundTo: alice,
+            deadline: block.timestamp
+        });
+
+        vm.startPrank(alice);
+        token.approve(address(lbRouter), type(uint256).max);
+        wNative.approve(address(lbRouter), type(uint256).max);
+
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                ILBRouter.LBRouter__AmountSlippageCaught.selector,
+                100_000e18,
+                100_000e18,
+                100_000e18,
+                99999999999999999939182
+            )
+        );
+        lbRouter.addLiquidity(params);
+
+        params.amountYMin = params.amountYMin * (1e18 - 1) / 1e18;
+        lbRouter.addLiquidity(params);
+        vm.stopPrank();
+    }
+
     function _swapNbBins(ILBPair lbPair, bool swapForY, uint24 nbBin) internal {
         require(nbBin > 0, "TestHelper: nbBin must be > 0");
 
@@ -251,12 +382,20 @@ contract MockFloorToken is ERC20, FloorToken {
         _transferOwnership(owner);
     }
 
+    function balanceOf(address account) public view override(ERC20, FloorToken) returns (uint256) {
+        return ERC20.balanceOf(account);
+    }
+
     function totalSupply() public view override(ERC20, FloorToken) returns (uint256) {
         return ERC20.totalSupply();
     }
 
     function _mint(address account, uint256 amount) internal override(ERC20, FloorToken) {
         ERC20._mint(account, amount);
+    }
+
+    function _burn(address account, uint256 amount) internal override(ERC20, FloorToken) {
+        ERC20._burn(account, amount);
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, FloorToken) {
